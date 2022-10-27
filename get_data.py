53224509a94a10fc37263ef1810dacaaad1b5777
Ostrovsky <ostrovskiy.e@list.ru>
2022-10-27 22:59:12 +0300

    Добавил обработку исключений, логи

--- get_data.py
+++ get_data.py
@@ -7,12 +7,43 @@ from modules import FaceitApi
 import asyncio
 import httpx
 
+
 token = settings.API_KEY
 db_name = settings.DBNAME
 db_user = settings.USERDB
 db_password = settings.PASSWORDDB
 db_host = settings.ADRESSDB
 db_port = settings.PORTDB
+
+map_stats_columns = ['map_cs','player_id','KD','Average_Quadro_Kills','Average_MVPs','Average_Assists',
+					 'Penta_Kills','Headshots','Average_Kills','Quadro_Kills','Wins','Win_Rate',
+					 'Matches','Triple_Kills','Headshots_per_Match','Average_KR','Average_Deaths',
+					 'Total_Headshots','KR','Average_Triple_Kills','Average_Headshots','Rounds',
+					 'Assists','MVPs','Kills','Deaths','Average_Penta_Kills','Average_KD','time_parse']
+players_previous_stats_columns = ['match_id','player_id','Score','Overtime_score',
+								 'First_Half_Score','Second_Half_Score','Final_Score',
+								  'map_csgo','Deaths','Headshots_per','Headshots','Kills','KD',
+								  'Penta_Kills','Quadro_Kills','Triple_Kills','MVPs','KR','Assists','result_match']
+columnsdb_life_stats = {
+	'Current Win Streak':'Current_Win_Streak',
+	'Average Headshots %': 'Average_Headshots',
+	'Matches':'Matches',
+	'Average K/D Ratio':'Average_KD',
+	'Win Rate %':'Win_Rate',
+	'Wins':'Wins',
+	'Longest Win Streak':'Longest_Win_Streak',
+	'Total Headshots %':'Total_Headshots',
+	'player_id':'player_id',
+	'elo':'elo',
+	'level':'faceit_level',
+	'time_parse':'time_parse'
+}
+query = """
+		select 
+			distinct mim.match_id
+		from 
+			raw_data.meta_info_match mim
+		"""
 
 def create_connection(db_name, db_user, db_password, db_host, db_port):
 	connection = None
@@ -40,7 +71,6 @@ def execute_read_query(connection, query):
 	except OperationalError as e:
 		print(f"The error '{e}' occurred")
 
-
 def insert_values(connection, schema='railway', table='', values=list(), columns=list()):
 	try:
 		records = ", ".join(["%s"] * len(values))
@@ -53,9 +83,11 @@ def insert_values(connection, schema='railway', table='', values=list(), columns
 		cursor = connection.cursor()
 		cursor.execute(insert_query, values)
 		print(f'Записали данные в {table}')
-		return 200
 	except Exception as e:
-		return 404,e
+		print(f'{time.ctime()} {e} insert_values')
+		f = open('errors.txt', 'a+')
+		f.write(f'{time.ctime()} {e} insert_values'+'\n')
+		f.close()
 
 async def get_stats(last_matches,player):
 	async with httpx.AsyncClient() as client:
@@ -64,7 +96,8 @@ async def get_stats(last_matches,player):
 			stats.append(asyncio.create_task(fa.get_stats_of_match(client,prev_match,player)))
 										
 		stats_previos_match = await asyncio.gather(*stats)
-		return stats_previos_match
+		values = list(filter((704).__ne__, stats_previos_match))
+		return values
 
 async def get_stats_speed(last_matches,player):
 	async with httpx.AsyncClient() as client:
@@ -117,40 +150,15 @@ async def get_info(players,timestamp_to,was_parsing,next_matches):
 
 				insert_values(connection,schema='raw_data',table='map_stats',values=map_stats,columns=map_stats_columns)
 	
-	return [next_matches,prev_match_20_gather]
+	return [next_matches,prev_match_20_gather]		
 
-map_stats_columns = ['map_cs','player_id','KD','Average_Quadro_Kills','Average_MVPs','Average_Assists',
-					 'Penta_Kills','Headshots','Average_Kills','Quadro_Kills','Wins','Win_Rate',
-					 'Matches','Triple_Kills','Headshots_per_Match','Average_KR','Average_Deaths',
-					 'Total_Headshots','KR','Average_Triple_Kills','Average_Headshots','Rounds',
-					 'Assists','MVPs','Kills','Deaths','Average_Penta_Kills','Average_KD','time_parse']
 
-players_previous_stats_columns = ['match_id','player_id','Score','Overtime_score',
-								 'First_Half_Score','Second_Half_Score','Final_Score',
-								  'map_csgo','Deaths','Headshots_per','Headshots','Kills','KD',
-								  'Penta_Kills','Quadro_Kills','Triple_Kills','MVPs','KR','Assists','result_match']
-
-next_matches = ['1-b94d0662-d21b-4308-8576-ac698b9d30a8']
-print(next_matches)
-columnsdb_life_stats = {
-	'Current Win Streak':'Current_Win_Streak',
-	'Average Headshots %': 'Average_Headshots',
-	'Matches':'Matches',
-	'Average K/D Ratio':'Average_KD',
-	'Win Rate %':'Win_Rate',
-	'Wins':'Wins',
-	'Longest Win Streak':'Longest_Win_Streak',
-	'Total Headshots %':'Total_Headshots',
-	'player_id':'player_id',
-	'elo':'elo',
-	'level':'faceit_level',
-	'time_parse':'time_parse'
-}                            
-		
-					
 connection = create_connection(db_name,db_user,db_password,db_host,db_port)
+was_parsing = [x[0] for x in execute_read_query(connection,query)]
 fa = FaceitApi.FaceitApi(token)
-was_parsing = []
+connection = create_connection(db_name,db_user,db_password,db_host,db_port)
+next_matches = ['1-44688bae-b4cc-4410-97fe-a9a5c81bae8c']
+
 
 for match_id in next_matches:
 	was_parsing.append(match_id)
@@ -170,61 +178,10 @@ for match_id in next_matches:
 		# 	previos_matches += z[0]
 		# stats_previos_match = asyncio.run(get_stats_speed(last_matches = previos_matches, player = previos_players))
 
-		prev_gather_matchs = asyncio.run(get_info(players = players_id,timestamp_to=timestamp_to,was_parsing = was_parsing,next_matches = next_matches))
-		for gather_match in prev_gather_matchs[1]:
+		prev_gather_matches = asyncio.run(get_info(players = players_id,timestamp_to=timestamp_to,was_parsing = was_parsing,next_matches = next_matches))
+		for gather_match in prev_gather_matches[1]:
 			if gather_match[0]!=702:
 				stats_previos_match = asyncio.run(get_stats(last_matches = gather_match[0], player = gather_match[1]))
-				for i in stats_previos_match:
-					if i ==704:
-						stats_previos_match.remove(i)
-
 				insert_values(connection,schema='raw_data',table='players_previous_stats',
-						values=stats_previos_match,columns=players_previous_stats_columns)
-	
-
-
-
-
-		# for player in players_id:
-		# 	map_stats = []
-		# 	all_stats = fa.get_all_statistics(player)
-		# 	if all_stats!=705:
-		# 		life_stats = all_stats[0]
-		# 		life_stats_columns = []
-		# 		life_stats_values = [tuple(life_stats.values())]
-		# 		for key,value in life_stats.items():
-		# 			life_stats_columns.append(columnsdb_life_stats[key])
-
-		# 		insert_values(connection,schema='raw_data',table='life_stats',
-		# 					  values=life_stats_values,columns=life_stats_columns)
-
-
-		# 		for key,value in all_stats[1].items():
-		# 			map_stats.append(tuple([key]+
-		# 				[value['player_id'],value['K/D Ratio'],value['Average Quadro Kills'],value['Average MVPs'],
-		# 				value['Average Assists'],value['Penta Kills'],value['Headshots'],value['Average Kills'],
-		# 				value['Quadro Kills'],value['Wins'],value['Win Rate %'],value['Matches'],value['Triple Kills'],
-		# 				value['Headshots per Match'],value['Average K/R Ratio'],value['Average Deaths'],value['Total Headshots %'],
-		# 				value['K/R Ratio'],value['Average Triple Kills'],value['Average Headshots %'],value['Rounds'],
-		# 				value['Assists'],value['MVPs'],value['Kills'],value['Deaths'],value['Average Penta Kills'],value['Average K/D Ratio'],
-		# 				value['time_parse']]))
-
-		# 		insert_values(connection,schema='raw_data',table='map_stats',values=map_stats,columns=map_stats_columns)
-
-		# 	last_matches = fa.get_last_matches_id(player,timestamp_to)
-
-		# 	if last_matches!=702:
-		# 		stats_previos_match = asyncio.run(get_stats(last_matches, player))
-		# 		for i in stats_previos_match:
-		# 			if i ==704:
-		# 				stats_previos_match.remove(i)
-
-		# 		insert_values(connection,schema='raw_data',table='players_previous_stats',
-		# 				values=stats_previos_match,columns=players_previous_stats_columns)
-
-		# 		last_5_match = fa.get_last_5_match(player)
-		# 		if last_5_match!=703:
-		# 			for m in last_5_match:
-		# 				if (m not in was_parsing) and (m not in next_matches):
-		# 					next_matches.append(m)
+				 		values=stats_previos_match,columns=players_previous_stats_columns)
 
--- modules/FaceitApi.py
+++ modules/FaceitApi.py
@@ -37,42 +37,70 @@ class FaceitApi():
             return [players_id,meta_stats,res['started_at']]
         
         except Exception as e:
+            print(f'{time.ctime()} {response}: {e} get_players_id_and_meta_stats    match_id: {match_id}')
+            f = open('errors.txt', 'a+')
+            f.write(f'{time.ctime()} {response}: {e} get_players_id_and_meta_stats    match_id: {match_id}'+'\n')
+            f.close()
             if response.status_code == 429:
-                print('Слишком много запросов',response.status_code)
+                print(f'{time.ctime()} get_players_id_and_meta_stats Слишком много запросов,{response.status_code}')
                 time.sleep(30)
-            else:
-                print(f'{response}: {e} get_players_id_and_meta_stats    match_id: {match_id}')
-                f = open('errors.txt', 'a+')
-                f.write(f'{response}: {e} get_players_id_and_meta_stats    match_id: {match_id}'+'\n')
-                f.close()
             return 701
     
     async def get_last_matches_id(self,client,player_id,to,game='csgo',limit=20):
         """Получение id игр у игрока"""
         try:
+            response = None
             id_matches = []
             api_url = F"{self.__base_url}/players/{player_id}/history?game={game}&limit={limit}&to={to}"
-            #response = requests.get(api_url, headers=self.__headers)
             response = await client.get(api_url, headers=self.__headers)
             res = response.json()
             for match in res['items']:
                 if match['game_mode']=='5v5':
                     id_matches.append(match['match_id'])
             return [id_matches,player_id]
+
         except Exception as e:
-            if response.status_code == 429:
-                print('Слишком много запросов')
-                time.sleep(30)
-            else:
-                print(f'{response}: {e} get_last_matches_id    player_id: {player_id}')
-                f = open('errors.txt', 'a+')
-                f.write(f'{response}: {e} get_last_matches_id    player_id: {player_id}'+'\n')
-                f.close()
-            return 702
+            if response == None:
+                try:
+                    time.sleep(0.05)
+                    response = None
+                    id_matches = []
+                    api_url = F"{self.__base_url}/players/{player_id}/history?game={game}&limit={limit}&to={to}"
+                    response = await client.get(api_url, headers=self.__headers)
+                    res = response.json()
+                    for match in res['items']:
+                        if match['game_mode']=='5v5':
+                            id_matches.append(match['match_id'])
+                    return [id_matches,player_id]
+                except:
+                    print(f'{time.ctime()}    {response}: {e} get_last_matches_id    player_id: {player_id}')
+                    f = open('errors.txt', 'a+')
+                    f.write(f'{time.ctime()}    {response}: {e} get_last_matches_id    player_id: {player_id}'+'\n')
+                    f.close()
+                    return [702,player_id]
+            elif response!=None:
+                try:
+                    time.sleep(0.05)
+                    response = None
+                    id_matches = []
+                    api_url = F"{self.__base_url}/players/{player_id}/history?game={game}&limit={limit}&to={to}"
+                    response = await client.get(api_url, headers=self.__headers)
+                    res = response.json()
+                    for match in res['items']:
+                        if match['game_mode']=='5v5':
+                            id_matches.append(match['match_id'])
+                    return [id_matches,player_id]
+                except:
+                    print(f'{time.ctime()}    {response}: {e} get_last_matches_id    player_id: {player_id}')
+                    f = open('errors.txt', 'a+')
+                    f.write(f'{time.ctime()}    {response}: {e} get_last_matches_id    player_id: {player_id}'+'\n')
+                    f.close()
+                    return [702,player_id]
 
     async def get_last_5_match(self,client,player_id,game='csgo',limit=2):
         
         try:
+            response=None
             id_matches = []
             api_url = F"{self.__base_url}/players/{player_id}/history?game={game}&limit={limit}"
             response = await client.get(api_url, headers=self.__headers)
@@ -83,20 +111,20 @@ class FaceitApi():
             return id_matches
 
         except Exception as e:
-            if response.status_code == 429:
-                print('Слишком много запросов')
-                time.sleep(30)
-            else:
-                print(f'{response}: {e} get_last_5_match    player_id: {player_id}')
+            if response == None:
+                print(f'{time.ctime()}    {response}: {e} get_last_5_match    player_id: {player_id}')
                 f = open('errors.txt', 'a+')
-                f.write(f'{response}: {e} get_last_5_match    player_id: {player_id}'+'\n')
+                f.write(f'{time.ctime()}    {response}: {e} get_last_5_match    player_id: {player_id}'+'\n')
                 f.close()
+            elif response.status_code == 429:
+                print(f'{time.ctime()} get_last_5_match Слишком много запросов,{response.status_code}')
+                time.sleep(30)
             return 703
     
     
     async def get_stats_of_match(self,client,match_id,player_id):
         try:
-            players_stats=''
+            response = None
             api_url_stats = F"{self.__base_url}/matches/{match_id}/stats"
             response = await client.get(api_url_stats, headers=self.__headers)
             res_stats = response.json()['rounds'][0]
@@ -115,21 +143,48 @@ class FaceitApi():
                             ])
                         break
             return players_stats
+
         except Exception as e:
-            if response.status_code == 429:
-                print('Слишком много запросов')
-                time.sleep(30)
-            else:
-                print(f'{response}: {e} get_stats_of_match    player_id: {player_id};    match_id: {match_id}')
+            if response!=None:
+                print(f'{time.ctime()}    {response}: {e} get_stats_of_match    player_id: {player_id};    match_id: {match_id}')
                 f = open('errors.txt', 'a+')
-                f.write(f'{response}: {e} get_stats_of_match    player_id: {player_id};    match_id: {match_id}'+'\n')
+                f.write(f'{time.ctime()}    {response}: {e} get_stats_of_match    player_id: {player_id};    match_id: {match_id}'+'\n')
                 f.close()
-            return 704
-    
+                return 704
+            elif response==None:
+                print(f'{time.ctime()}:    response not created    {e} get_stats_of_match    player_id: {player_id};    match_id: {match_id}')
+                f = open('errors.txt', 'a+')
+                f.write(f'{time.ctime()}:    response not created    {e} get_stats_of_match    player_id: {player_id};    match_id: {match_id}'+'\n')
+                f.close()
+                try:
+                    time.sleep(0.05)
+                    api_url_stats = F"{self.__base_url}/matches/{match_id}/stats"
+                    response = await client.get(api_url_stats, headers=self.__headers)
+                    res_stats = response.json()['rounds'][0]
+                    for team in res_stats['teams']:
+                        for player in team['players']:
+                            if player['player_id']==player_id:
+                                players_stats = tuple([res_stats['match_id'],player['player_id'],res_stats['round_stats']['Score'],
+                                     team['team_stats']['Overtime score'],team['team_stats']['First Half Score'],
+                                     team['team_stats']['Second Half Score'],team['team_stats']['Final Score'],res_stats['round_stats']['Map'],
+                                    player['player_stats']['Deaths'],player['player_stats']['Headshots %'],
+                                     player['player_stats']['Headshots'],player['player_stats']['Kills'],
+                                    player['player_stats']['K/D Ratio'],player['player_stats']['Penta Kills'],
+                                     player['player_stats']['Quadro Kills'],player['player_stats']['Triple Kills'],
+                                     player['player_stats']['MVPs'],player['player_stats']['K/R Ratio'],player['player_stats']['Assists'],
+                                     player['player_stats']['Result']
+                                    ])
+                                break
+                    return players_stats
+                except:
+                    print('Повторный запрос не сработал')
+                    return 704
+
     async def get_all_statistics(self,client,player_id):
         
         try:
             map_stats = {}
+            res1,res2 = None,None
             tasks = [F"{self.__base_url}/players/{player_id}",
                     F"{self.__base_url}/players/{player_id}/stats/csgo"]
             res1 = await client.get(tasks[0], headers=self.__headers)
@@ -153,100 +208,17 @@ class FaceitApi():
                     map_stats[map_cs['label']]['time_parse'] = timestamp
             
             return [res2_life,map_stats]
+
         except Exception as e:
-            if res2.status_code == 429 or res2.status_code == 429:
-                print('Слишком много запросов')
-                time.sleep(30)
-            else:
-                print(f'response_1: {res1}    response_2: {res2}:    {e}    get_all_statistics    player_id: {player_id}')
+            if res1==None or res2==None:
+                print(f'{time.ctime()}    response_1: {res1}    response_2: {res2}:    {e}    get_all_statistics    player_id: {player_id}')
                 f = open('errors.txt', 'a+')
-                f.write(f'response_1: {res1}    response_2: {res2}:    {e}    get_all_statistics    player_id: {player_id}'+'\n')
+                f.write(f'{time.ctime()}    response_1: {res1}    response_2: {res2}:    {e}    get_all_statistics    player_id: {player_id}'+'\n')
                 f.close()
-            return 705
-
-            # def get_last_5_match(self,player_id,game='csgo',limit=2):
-        
-    #     try:
-    #         id_matches = []
-    #         api_url = F"{self.__base_url}/players/{player_id}/history?game={game}&limit={limit}"
-    #         response = requests.get(api_url, headers=self.__headers)
-    #         res = response.json()
-    #         for match in res['items']:
-    #             if match['game_mode']=='5v5':
-    #                 id_matches.append(match['match_id'])
-    #         return id_matches
-
-    #     except Exception as e:
-    #         if response.status_code == 429:
-    #             print('Слишком много запросов')
-    #             time.sleep(30)
-    #         else:
-    #             print(f'{response}: {e} get_last_5_match    player_id: {player_id}')
-    #             f = open('errors.txt', 'a+')
-    #             f.write(f'{response}: {e} get_last_5_match    player_id: {player_id}'+'\n')
-    #             f.close()
-    #         return 703
-
-    # def get_last_matches_id(self,player_id,to,game='csgo',limit=20):
-    #     """Получение id игр у игрока"""
-    #     try:
-    #         id_matches = []
-    #         api_url = F"{self.__base_url}/players/{player_id}/history?game={game}&limit={limit}&to={to}"
-    #         response = requests.get(api_url, headers=self.__headers)
-    #         res = response.json()
-    #         for match in res['items']:
-    #             if match['game_mode']=='5v5':
-    #                 id_matches.append(match['match_id'])
-    #         return id_matches
-    #     except Exception as e:
-    #         if response.status_code == 429:
-    #             print('Слишком много запросов')
-    #             time.sleep(30)
-    #         else:
-    #             print(f'{response}: {e} get_last_matches_id    player_id: {player_id}')
-    #             f = open('errors.txt', 'a+')
-    #             f.write(f'{response}: {e} get_last_matches_id    player_id: {player_id}'+'\n')
-    #             f.close()
-    #         return 702
-
-    # def get_all_statistics(self,player_id):
-        
-    #     try:
-    #         map_stats = {}
-    #         res=[]
-    #         tasks = [F"{self.__base_url}/players/{player_id}",
-    #                 F"{self.__base_url}/players/{player_id}/stats/csgo"]
-    #         with httpx.Client(headers=self.__headers) as client:
-    #             for api_url in tasks:
-    #                 response = client.get(api_url)
-    #                 res.append(response)
-            
-    #         res1,res2 = res[0],res[1]
-    #         level = res1.json()['games']['csgo']['skill_level']
-    #         elo = res1.json()['games']['csgo']['faceit_elo']
-
-    #         res2_life,res2_segm = res2.json()['lifetime'], res2.json()['segments']
-    #         del res2_life['K/D Ratio']
-    #         del res2_life['Recent Results']
-    #         timestamp = time.time()
-    #         res2_life['player_id'] = player_id
-    #         res2_life['elo'] = elo
-    #         res2_life['level'] = level
-    #         res2_life['time_parse'] = timestamp
-    #         for map_cs in res2_segm:
-    #             if map_cs['type']=='Map' and map_cs['mode']=='5v5':
-    #                 map_stats[map_cs['label']] = map_cs['stats']
-    #                 map_stats[map_cs['label']]['player_id'] = player_id
-    #                 map_stats[map_cs['label']]['time_parse'] = timestamp
-            
-    #         return [res2_life,map_stats]
-    #     except Exception as e:
-    #         if res2.status_code == 429 or res2.status_code == 429:
-    #             print('Слишком много запросов')
-    #             time.sleep(30)
-    #         else:
-    #             print(f'response_1: {res1}    response_2: {res2}:    {e}    get_all_statistics    player_id: {player_id}')
-    #             f = open('errors.txt', 'a+')
-    #             f.write(f'response_1: {res1}    response_2: {res2}:    {e}    get_all_statistics    player_id: {player_id}'+'\n')
-    #             f.close()
-    #         return 705
+                return 705
+            else:
+                print(f'{time.ctime()}    response_1: {res1}    response_2: {res2}:    {e}    get_all_statistics    player_id: {player_id}')
+                f = open('errors.txt', 'a+')
+                f.write(f'{time.ctime()}    response_1: {res1}    response_2: {res2}:    {e}    get_all_statistics    player_id: {player_id}'+'\n')
+                f.close()
+                return 705
